ad87b3104f1112e1dabdf485e7b3d4f513c5edf4
swapnataricoxbd-prog <swapnataricoxbd@gmail.com>
2026-06-08 13:20:16 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'svyo-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  '20260304_112626.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
